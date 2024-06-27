35d3c1814950abdb138d1405c54e7c28bdf32c56
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-06-27 10:47:09 +0200

    feat: add nodemon

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
 });
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
-const app = express();
-const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });

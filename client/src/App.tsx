104e8ca1a0c58f1a70e8477d375007c4fe50dbe6
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-06-27 18:20:46 +0200

    chore: Refactor App component and add Home page component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,69 +1,33 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import socketIO from "socket.io-client";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./page/Home";
 
-function App() {
-  const [socket] = useState(() => socketIO("http://localhost:3000"));
-  const [messages, setMessages] = useState("");
-  const [isConnected, setIsConnected] = useState(false);
-  const [receivedMessages, setReceivedMessages] = useState<any[]>([]);
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log(`⚡: ${socket.id} user just connected!`);
-      setIsConnected(true);
-    });
-
-    socket.on("message", (data) => {
-      console.log("📩: Received message:", data);
-      setReceivedMessages((prevMessages) => [...prevMessages, data]);
-    });
-
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
-
-    // Cleanup on component unmount
-    return () => {
-      socket.off("connect");
-      socket.off("message");
-      socket.off("disconnect");
-    };
-  }, [socket]);
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    if (messages.length > 0) {
-      socket.emit("message", messages);
-      setMessages("");
-    }
-  };
-
+const Layout = () => {
   return (
     <>
-      <div>
-        <h1>Socket.IO Chat App</h1>
-        <div>
-          <ul>
-            {receivedMessages.map((message, index) => (
-              <li key={index}>{message}</li>
-            ))}
-          </ul>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter message"
-            onChange={(e) => setMessages(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
-        <p>
-          <strong>Status:</strong> {isConnected ? "Connected" : "Disconnected"}
-        </p>
-      </div>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
     </>
   );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;

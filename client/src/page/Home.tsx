7defe3b5e7bd685484bdeab4dc774de1ee280d5b
Jules Jean-Louis <jules_jean-louis@natural-solutions.eu>
2024-07-08 15:54:44 +0200

    feat: room

--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import socketIO from "socket.io-client";
 
-
-export const Home =() => {
+export const Home = () => {
   const [socket] = useState(() => socketIO("http://localhost:3000"));
   const [messages, setMessages] = useState("");
+  const [roomMessages, setRoomMessages] = useState("");
+  const [rooms, setRooms] = useState<string[]>([]);
+  const [roomName, setRoomName] = useState("");
   const [isConnected, setIsConnected] = useState(false);
   const [receivedMessages, setReceivedMessages] = useState<any[]>([]);
+  const [activeRoomMsg, setActiveRoomMsg] = useState<any[]>([]);
 
   useEffect(() => {
     socket.on("connect", () => {
@@ -14,9 +17,18 @@ export const Home =() => {
       setIsConnected(true);
     });
 
+    socket.on("activeRooms", (data) => {
+      setRooms(data);
+    });
+
     socket.on("message", (data) => {
       console.log("📩: Received message:", data);
       setReceivedMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    socket.on("msg", (data) => {
+      console.log("📩: Received message:", data);
+      setActiveRoomMsg((prevMessages) => [...prevMessages, data]);
     });
 
     socket.on("disconnect", () => {
@@ -27,6 +39,8 @@ export const Home =() => {
     return () => {
       socket.off("connect");
       socket.off("message");
+      socket.off("msg");
+      socket.off("activeRooms");
       socket.off("disconnect");
     };
   }, [socket]);
@@ -39,29 +53,117 @@ export const Home =() => {
     }
   };
 
+  const handleJoinRoom = (e: any) => {
+    e.preventDefault();
+    socket.emit("joinRoom", roomName);
+  };
+
+  const handlePostMessage = () => {
+    if (!roomMessages || !roomName) return;
+    console.log("sending message");
+    socket.emit("msg", { roomName: roomName, message: roomMessages });
+  };
+
+
   return (
     <>
-      <div>
-        <h1>Socket.IO Chat App</h1>
-        <div>
-          <ul>
-            {receivedMessages.map((message, index) => (
-              <li key={index}>{message}</li>
-            ))}
-          </ul>
+      <section className="flex items-start w-screen justify-between h-screen space-x-4">
+        <div className="bg-green-100 px-3 w-[10.5rem] h-full">
+          {rooms.length > 0 ? (
+            <ul>
+              {rooms.map((room, index) => (
+                <li key={index} className="flex space-x-1">
+                  <button
+                    onClick={() => {
+                      socket.emit("joinRoom", `${room}`);
+                      setRoomName(room);
+                    }}
+                  >
+                    {room}
+                  </button>
+                  <button
+                    onClick={() => {
+                      socket.emit("leaveRoom", `${room}`);
+                    }}
+                    className="text-red-500 border border-red-500 rounded-md px-2 py-1"
+                  >
+                    Leave
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No active rooms</p>
+          )}
+          <form
+            action=""
+            method="post"
+            onSubmit={(e) => handleJoinRoom(e)}
+            className="flex flex-col"
+          >
+            <input
+              type="text"
+              placeholder="Enter room name"
+              onChange={(e) => setRoomName(e.target.value)}
+              className="mb-2 w-fit"
+            />
+            <button type="submit">Create Room</button>
+          </form>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter message"
-            onChange={(e) => setMessages(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
-        <p>
-          <strong>Status:</strong> {isConnected ? "Connected" : "Disconnected"}
-        </p>
-      </div>
+        <div className="w-1/2 bg-green-200">
+          <h2>Room</h2>
+          <div className="flex flex-col w-full">
+            <div className="flex flex-col w-full min-h-72">
+              <ul>
+                {activeRoomMsg.map((msg, index) => (
+                  <li key={index}>
+                    <p>{msg.message}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <form
+                action=""
+                method="post"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handlePostMessage();
+                }}
+              >
+                <input
+                  type="text"
+                  placeholder="Enter message"
+                  onChange={(e) => setRoomMessages(e.target.value)}
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1>Socket.IO Chat App</h1>
+          <div>
+            <ul>
+              {receivedMessages.map((message, index) => (
+                <li key={index}>{message}</li>
+              ))}
+            </ul>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter message"
+              onChange={(e) => setMessages(e.target.value)}
+            />
+            <button type="submit">Send</button>
+          </form>
+          <p>
+            <strong>Status:</strong>{" "}
+            {isConnected ? "Connected" : "Disconnected"}
+          </p>
+        </div>
+      </section>
     </>
   );
-}
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -25,20 +25,67 @@ const io = new SocketIOServer(server, {
 app.use(cors());
 app.use(express.json());
 
+const getActiveRooms = () => {
+  const rooms = io.sockets.adapter.rooms;
+  const roomList: string[] = [];
+  rooms.forEach((_, key) => {
+    if (!io.sockets.sockets.has(key)) {
+      roomList.push(key);
+    }
+  });
+  return roomList;
+};
+
+const updateActiveRooms = () => {
+  const rooms = getActiveRooms();
+  io.emit("activeRooms", rooms);
+};
+
 io.on("connection", (socket: Socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
+
+  // Rejoindre une room
+  socket.on("joinRoom", (roomName: string) => {
+    socket.join(roomName);
+    io.to(roomName).emit("msg", {
+      roomName: roomName,
+      message: `User ${socket.id} has joined room ${roomName}`,
+    });
+    console.log(`User ${socket.id} has joined room ${roomName}`);
+    updateActiveRooms();
+  });
+
+  // Quitter une room
+  socket.on("leaveRoom", (roomName: string) => {
+    socket.leave(roomName);
+    io.to(roomName).emit("msg", {
+      roomName: roomName,
+      message: `User ${socket.id} has joined room ${roomName}`,
+    });
+    console.log(`User ${socket.id} has left room ${roomName}`);
+    updateActiveRooms();
+  });
+
+  socket.on("getRooms", () => {
+    const rooms = getActiveRooms();
+    socket.emit("activeRooms", rooms);
+  });
+
+  const rooms = getActiveRooms();
+  socket.emit("activeRooms", rooms);
+
+  socket.on("msg", (data: { roomName: string; message: string }) => {
+    console.log(
+      `📩: Received message for room ${data.roomName}: ${data.message}`
+    );
+    io.to(data.roomName).emit("msg", { message: data.message });
+  });
 
   socket.on("message", (data: any) => {
     console.log("📩: Received message:", data);
     io.emit("message", data);
   });
-  io.emit('userConnected', { message: `New user connected: ${socket.id}` });
-
-  // socket.on('customEvent', (data: any) => {
-  //   console.log('Received custom event:', data);
-
-  //   socket.emit('customResponse', { message: 'Event received successfully' });
-  // });
+  io.emit("userConnected", { message: `New user connected: ${socket.id}` });
 
   socket.on("disconnect", () => {
     console.log("🔥: A user disconnected");
@@ -54,21 +101,21 @@ app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "Hello world" });
 });
 
-app.post("/api/user/create", async (req: Request, res: Response) => {
-  try {
-    const result = await createUser(req.body);
-    if ("error" in result) {
-      res.status(400).json({ error: result.error });
-    } else {
-      res
-        .status(201)
-        .json({ message: "User created successfully", user: result });
-    }
-  } catch (error) {
-    console.error("Error in /api/user/create route", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+// app.post("/api/user/create", async (req: Request, res: Response) => {
+//   try {
+//     const result = await createUser(req.body);
+//     if ("error" in result) {
+//       res.status(400).json({ error: result.error });
+//     } else {
+//       res
+//         .status(201)
+//         .json({ message: "User created successfully", user: result });
+//     }
+//   } catch (error) {
+//     console.error("Error in /api/user/create route", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
 app.get("/api/users/", async (req: Request, res: Response) => {
   const result = findAllUsers();
